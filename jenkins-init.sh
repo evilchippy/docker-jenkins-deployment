97bf5a71cf05273b51acaa99513e99aaa2ed3fbb
Michael Liberman <admin@yourfurniturenow.com>
2018-05-18 19:02:18 -0700

    fix

--- jenkins-init.sh
+++ jenkins-init.sh
@@ -1,8 +1,5 @@
-# Adding user jenkins to docker group
 usermod -a -G $(ls -la /var/run/docker.sock | awk '{ print $4 }') jenkins
 
-# Change ownership of jenkins_home 
 chown -R jenkins:jenkins /var/jenkins_home 
 
-# Run standard jenkins startup script as jenkins
 su -c /usr/local/bin/jenkins.sh jenkins
