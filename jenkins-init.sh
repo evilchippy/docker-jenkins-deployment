93eec8817ce39242a5cb083c45fdb47eb3017933
Michael Liberman <admin@yourfurniturenow.com>
2018-05-19 08:51:42 -0700

    undo

--- jenkins-init.sh
+++ jenkins-init.sh
@@ -2,4 +2,4 @@ usermod -a -G $(ls -la /var/run/docker.sock | awk '{ print $4 }') jenkins
 
 chown -R jenkins:jenkins /var/jenkins_home 
 
-#su -c /usr/local/bin/jenkins.sh jenkins
+su -c /usr/local/bin/jenkins.sh jenkins
